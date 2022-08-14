5676bfb0f4e95f2d0f48716976fb9188eb469190
Stella Kariuki <78648887+skariuki98@users.noreply.github.com>
2022-08-13 20:27:38 -0400

    Create FSM GCD
    ..

---
+++ FSM GCD/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/kariu/Documents/Spring 2022/CDA4203L/Projects/Lab5/project5GCD/GCDtestbench_isim_beh.exe" -prj "C:/Users/kariu/Documents/Spring 2022/CDA4203L/Projects/Lab5/project5GCD/GCDtestbench_beh.prj" "work.GCDtestbench" "work.glbl" 
